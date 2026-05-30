2d413b294f100a0e54f06007dc31de6c35ababf9
cantikkamay <cantikkamayaristianti@gmail.com>
2026-05-30 15:32:48 +0700

    Simulasi broken build

--- tests/CatalogTest.php
+++ tests/CatalogTest.php
@@ -37,7 +37,7 @@ class CatalogTest extends TestCase
         $result = $this->katalog
             ->searchProduct("Kemeja");
 
-        $this->assertCount(1, $result);
+        $this->assertCount(99, $result);
     }
 
     // UT-02
